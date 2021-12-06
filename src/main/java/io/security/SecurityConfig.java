08d806ecaeef47df313572bcda4708e9ccb7ae82
Yonghwi Kim <yh0921k@gmail.com>
2021-12-06 19:12:49 +0900

    mod: Spring Security Logout 코드 테스트

--- src/main/java/io/security/SecurityConfig.java
+++ src/main/java/io/security/SecurityConfig.java
@@ -5,13 +5,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Configuration
@@ -22,37 +22,33 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests().anyRequest().authenticated();
 
-    http.formLogin()
-        //.loginPage("/loginPage")
-        .defaultSuccessUrl("/")
-        .failureUrl("/login")
-        .usernameParameter("userId")
-        .passwordParameter("passwd")
-        .loginProcessingUrl("/login_proc")
-        .successHandler(
-            new AuthenticationSuccessHandler() {
+    http.formLogin();
+
+    http.logout()
+        .logoutUrl("/logout")
+        .logoutSuccessUrl("/login")
+        .addLogoutHandler(
+            new LogoutHandler() {
               @Override
-              public void onAuthenticationSuccess(
+              public void logout(
+                  HttpServletRequest request,
+                  HttpServletResponse response,
+                  Authentication authentication) {
+                HttpSession httpSession = request.getSession();
+                httpSession.invalidate();
+              }
+            })
+        .logoutSuccessHandler(
+            new LogoutSuccessHandler() {
+              @Override
+              public void onLogoutSuccess(
                   HttpServletRequest request,
                   HttpServletResponse response,
                   Authentication authentication)
                   throws IOException, ServletException {
-                System.out.println("authentication : " + authentication.getName());
-                response.sendRedirect("/");
-              }
-            })
-        .failureHandler(
-            new AuthenticationFailureHandler() {
-              @Override
-              public void onAuthenticationFailure(
-                  HttpServletRequest request,
-                  HttpServletResponse response,
-                  AuthenticationException exception)
-                  throws IOException, ServletException {
-                System.out.println("exception : " + exception.getMessage());
                 response.sendRedirect("/login");
               }
             })
-        .permitAll();
+        .deleteCookies("remember-me");
   }
 }
