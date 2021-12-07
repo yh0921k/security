0857b7d1fdc8b97d4394a8b1f65aa641b0893d5b
Yonghwi Kim <yh0921k@gmail.com>
2021-12-07 09:51:25 +0900

    mod: Remember Me 인증 관련 초기 예제

--- src/main/java/io/security/SecurityConfig.java
+++ src/main/java/io/security/SecurityConfig.java
@@ -1,10 +1,12 @@
 package io.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
@@ -17,6 +19,8 @@ import java.io.IOException;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Autowired UserDetailsService userDetailsService;
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
@@ -49,6 +53,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 response.sendRedirect("/login");
               }
             })
-        .deleteCookies("remember-me");
+        .deleteCookies("remember-me")
+        .and()
+        .rememberMe()
+        .rememberMeParameter("remember")
+        .tokenValiditySeconds(3600)
+        .userDetailsService(userDetailsService);
   }
 }
