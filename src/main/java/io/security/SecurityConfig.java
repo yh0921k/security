d30a4413bf63c744a9d1ddaecdaf52397e09f707
Yonghwi Kim <yh0921k@gmail.com>
2021-12-07 22:36:22 +0900

    mod: 인가 API - 권한 설정 및 표현식

--- src/main/java/io/security/SecurityConfig.java
+++ src/main/java/io/security/SecurityConfig.java
@@ -1,24 +1,37 @@
 package io.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-  @Autowired UserDetailsService userDetailsService;
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication().withUser("user").password("{noop}1111").roles("USER");
+    auth.inMemoryAuthentication().withUser("sys").password("{noop}1111").roles("SYS", "USER");
+    auth.inMemoryAuthentication()
+        .withUser("admin")
+        .password("{noop}1111")
+        .roles("ADMIN", "SYS", "USER");
+  }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests().anyRequest().authenticated();
+    http.authorizeRequests()
+        .antMatchers("/user")
+        .hasRole("USER")
+        .antMatchers("/admin/pay")
+        .hasRole("ADMIN")
+        .antMatchers("/admin/**")
+        .access("hasRole('ADMIN') or hasRole('SYS')")
+        .anyRequest()
+        .authenticated();
 
     http.formLogin();
-
-    http.sessionManagement().maximumSessions(1).maxSessionsPreventsLogin(true);
   }
 }
