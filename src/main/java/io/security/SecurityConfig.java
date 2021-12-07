3fa7bdf2b806502866bc9c588392d2d583f447bd
Yonghwi Kim <yh0921k@gmail.com>
2021-12-07 23:50:55 +0900

    mod: CSRF 필터

--- src/main/java/io/security/SecurityConfig.java
+++ src/main/java/io/security/SecurityConfig.java
@@ -1,91 +1,20 @@
 package io.security;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.access.AccessDeniedHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
-import org.springframework.security.web.savedrequest.RequestCache;
-import org.springframework.security.web.savedrequest.SavedRequest;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.inMemoryAuthentication().withUser("user").password("{noop}1111").roles("USER");
-    auth.inMemoryAuthentication().withUser("sys").password("{noop}1111").roles("SYS", "USER");
-    auth.inMemoryAuthentication()
-        .withUser("admin")
-        .password("{noop}1111")
-        .roles("ADMIN", "SYS", "USER");
-  }
-
-  @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests()
-        .antMatchers("/login")
-        .permitAll()
-        .antMatchers("/user")
-        .hasRole("USER")
-        .antMatchers("/admin/pay")
-        .hasRole("ADMIN")
-        .antMatchers("/admin/**")
-        .access("hasRole('ADMIN') or hasRole('SYS')")
-        .anyRequest()
-        .authenticated();
+    http.authorizeRequests().anyRequest().permitAll();
 
-    http.formLogin()
-        .successHandler(
-            new AuthenticationSuccessHandler() {
-              @Override
-              public void onAuthenticationSuccess(
-                  HttpServletRequest request,
-                  HttpServletResponse response,
-                  Authentication authentication)
-                  throws IOException, ServletException {
-                RequestCache requestCache = new HttpSessionRequestCache();
-                SavedRequest savedRequest = requestCache.getRequest(request, response);
-                String redirectUrl = savedRequest.getRedirectUrl();
-                response.sendRedirect(redirectUrl);
-              }
-            });
+    http.formLogin();
 
-    http.exceptionHandling()
-//        .authenticationEntryPoint(
-//            new AuthenticationEntryPoint() {
-//              @Override
-//              public void commence(
-//                  HttpServletRequest request,
-//                  HttpServletResponse response,
-//                  AuthenticationException authException)
-//                  throws IOException, ServletException {
-//                response.sendRedirect("/login");
-//              }
-//            })
-        .accessDeniedHandler(
-            new AccessDeniedHandler() {
-              @Override
-              public void handle(
-                  HttpServletRequest request,
-                  HttpServletResponse response,
-                  AccessDeniedException accessDeniedException)
-                  throws IOException, ServletException {
-                response.sendRedirect("/denied");
-              }
-            });
+    http.csrf().disable();
   }
 }
