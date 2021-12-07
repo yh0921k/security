54f10ce3aa9cc0bfe5a51c6b8a821f682ae0af9a
Yonghwi Kim <yh0921k@gmail.com>
2021-12-07 17:15:44 +0900

    mod: 동시 세션 제어

--- src/main/java/io/security/SecurityConfig.java
+++ src/main/java/io/security/SecurityConfig.java
@@ -5,16 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -28,36 +19,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     http.formLogin();
 
-    http.logout()
-        .logoutUrl("/logout")
-        .logoutSuccessUrl("/login")
-        .addLogoutHandler(
-            new LogoutHandler() {
-              @Override
-              public void logout(
-                  HttpServletRequest request,
-                  HttpServletResponse response,
-                  Authentication authentication) {
-                HttpSession httpSession = request.getSession();
-                httpSession.invalidate();
-              }
-            })
-        .logoutSuccessHandler(
-            new LogoutSuccessHandler() {
-              @Override
-              public void onLogoutSuccess(
-                  HttpServletRequest request,
-                  HttpServletResponse response,
-                  Authentication authentication)
-                  throws IOException, ServletException {
-                response.sendRedirect("/login");
-              }
-            })
-        .deleteCookies("remember-me")
-        .and()
-        .rememberMe()
-        .rememberMeParameter("remember")
-        .tokenValiditySeconds(3600)
-        .userDetailsService(userDetailsService);
+    http.sessionManagement().maximumSessions(1).maxSessionsPreventsLogin(false);
   }
 }
