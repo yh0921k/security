1cd16eac674fa964d93b515f3d46407a0953f95a
Yonghwi Kim <yh0921k@gmail.com>
2021-12-07 23:37:02 +0900

    mod: 인증/인가 예외 필터 및 요청정보 캐싱

--- src/main/java/io/security/SecurityConfig.java
+++ src/main/java/io/security/SecurityConfig.java
@@ -1,10 +1,24 @@
 package io.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -23,6 +37,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
+        .antMatchers("/login")
+        .permitAll()
         .antMatchers("/user")
         .hasRole("USER")
         .antMatchers("/admin/pay")
@@ -32,6 +48,44 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .anyRequest()
         .authenticated();
 
-    http.formLogin();
+    http.formLogin()
+        .successHandler(
+            new AuthenticationSuccessHandler() {
+              @Override
+              public void onAuthenticationSuccess(
+                  HttpServletRequest request,
+                  HttpServletResponse response,
+                  Authentication authentication)
+                  throws IOException, ServletException {
+                RequestCache requestCache = new HttpSessionRequestCache();
+                SavedRequest savedRequest = requestCache.getRequest(request, response);
+                String redirectUrl = savedRequest.getRedirectUrl();
+                response.sendRedirect(redirectUrl);
+              }
+            });
+
+    http.exceptionHandling()
+//        .authenticationEntryPoint(
+//            new AuthenticationEntryPoint() {
+//              @Override
+//              public void commence(
+//                  HttpServletRequest request,
+//                  HttpServletResponse response,
+//                  AuthenticationException authException)
+//                  throws IOException, ServletException {
+//                response.sendRedirect("/login");
+//              }
+//            })
+        .accessDeniedHandler(
+            new AccessDeniedHandler() {
+              @Override
+              public void handle(
+                  HttpServletRequest request,
+                  HttpServletResponse response,
+                  AccessDeniedException accessDeniedException)
+                  throws IOException, ServletException {
+                response.sendRedirect("/denied");
+              }
+            });
   }
 }
